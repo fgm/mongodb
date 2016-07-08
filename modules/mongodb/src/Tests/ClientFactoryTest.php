c331aec806806fe762056991647035c51b300621
Frederic G. MARAND <fgm@osinet.fr>
2016-07-08 17:07:18 +0200

    Driver: DatabaseFactory 100% S0 coverage.

--- modules/mongodb/src/Tests/ClientFactoryTest.php
+++ modules/mongodb/src/Tests/ClientFactoryTest.php
@@ -3,8 +3,6 @@
 namespace Drupal\mongodb\Tests;
 
 use Drupal\Component\Render\FormattableMarkup;
-use Drupal\Core\Site\Settings;
-use Drupal\KernelTests\KernelTestBase;
 use Drupal\mongodb\ClientFactory;
 use MongoDB\Driver\Exception\ConnectionTimeoutException;
 
@@ -13,52 +11,7 @@ use MongoDB\Driver\Exception\ConnectionTimeoutException;
  *
  * @group MongoDB
  */
-class ClientFactoryTest extends KernelTestBase {
-  const DEFAULT_URI = 'mongodb://localhost:27017';
-  const BAD_ALIAS = 'bad';
-  const TEST_ALIAS = 'test';
-
-  public static $modules = ['mongodb'];
-
-  /**
-   * A test-specific instance of Settings.
-   *
-   * @var \Drupal\Core\Site\Settings
-   */
-  protected $settings;
-
-
-  protected $uri;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setUp() {
-    parent::setUp();
-    // $_ENV if it comes from phpunit.xml <env>
-    // $_SERVER if it comes from the phpunit command line environment
-    $this->uri = $_ENV['MONGODB_URL'] ?? $_SERVER['MONGODB_URI'] ?? static::DEFAULT_URI;
-
-    $this->settings = new Settings([
-      'mongodb' => [
-        'clients' => [
-          static::BAD_ALIAS => [
-            'uri' => 'mongodb://localhost:80',
-            'uriOptions' => [],
-            'driverOptions' => [],
-          ],
-          static::TEST_ALIAS => [
-            'uri' => $this->uri,
-            'uriOptions' => [],
-            'driverOptions' => [],
-          ],
-        ],
-        'databases' => [
-          'default' => [static::TEST_ALIAS, $this->getDatabasePrefix()],
-        ],
-      ],
-    ]);
-  }
+class ClientFactoryTest extends MongoDbTestBase {
 
   /**
    * Test a normal client creation attempt.
@@ -67,7 +20,7 @@ class ClientFactoryTest extends KernelTestBase {
     $cf = new ClientFactory($this->settings);
 
     try {
-      $client = $cf->get(static::TEST_ALIAS);
+      $client = $cf->get(static::CLIENT_TEST_ALIAS);
       // Force connection attempt by executing a command.
       $client->listDatabases();
     }
@@ -89,13 +42,13 @@ class ClientFactoryTest extends KernelTestBase {
     $cf = new ClientFactory($this->settings);
 
     try {
-      $client = $cf->get(static::BAD_ALIAS);
+      $client = $cf->get(static::CLIENT_BAD_ALIAS);
       // Force connection attempt by executing a command.
       $client->listDatabases();
       $this->fail("Should not have been able to connect to a non-server.");
     }
     catch (ConnectionTimeoutException $e) {
-      $this->pass("Cannot create a client to a non-server.");
+      $this->assertTrue(TRUE, "Cannot create a client to a non-server.");
     }
     catch (\Exception $e) {
       $this->fail($e->getMessage());
