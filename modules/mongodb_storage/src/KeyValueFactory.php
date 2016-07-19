915ee470e96aa7e0ce307b29de7d89c4501cd309
Frederic G. MARAND <fgm@osinet.fr>
2016-07-19 20:20:59 +0200

    A simple, yet apparently working KeyValueStore implementation.

---
+++ modules/mongodb_storage/src/KeyValueFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\mongodb_storage;
+
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+use Drupal\mongodb\DatabaseFactory;
+
+/**
+ * Class KeyValueFactory builds KeyValue stores as MongoDB collections.
+ */
+class KeyValueFactory implements KeyValueFactoryInterface {
+  const DATABASE_ALIAS = 'default';
+  const COLLECTION_PREFIX = 'keyvalue_';
+
+  /**
+   * The database in which the stores are created.
+   *
+   * @var \MongoDB\Database
+   */
+  protected $database;
+
+  /**
+   * A static cache for the stores.
+   *
+   * @var array<string,\Drupal\mongodb_storage\KeyValueStore>
+   */
+  protected $stores = [];
+
+  /**
+   * KeyValueFactory constructor.
+   *
+   * @param \Drupal\mongodb\DatabaseFactory $databaseFactory
+   *   The mongodb.database_factory service.
+   */
+  public function __construct(DatabaseFactory $databaseFactory) {
+    $this->database = $databaseFactory->get(static::DATABASE_ALIAS);
+  }
+
+  /**
+   * Constructs a new key/value store for a given collection name.
+   *
+   * @param string $collection
+   *   The name of the collection holding key and value pairs.
+   *
+   * @return \Drupal\Core\KeyValueStore\KeyValueStoreInterface
+   *   A key/value store implementation for the given $collection.
+   */
+  public function get($collection) {
+    $store_collection = $this->database->selectCollection(static::COLLECTION_PREFIX . $collection);
+    $store = new KeyValueStore($store_collection);
+    return $store;
+  }
+
+}
