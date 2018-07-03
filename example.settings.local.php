061ad5dd2397edb10a3ed07dcf1269d037b2705f
Frederic G. MARAND <fgm@osinet.fr>
2018-07-03 16:56:29 +0100

    Issue #2982218 by swamy, fgm: README improvements.
    - Github issue #33

--- example.settings.local.php
+++ example.settings.local.php
@@ -4,20 +4,29 @@
  * Example settings to connect to MongoDB.
  *
  * This is the default data to add to your settings.local.php.
+ *
+ * Using this format instead of raw settings enables easier enabling/disabling
+ * of the MongoDB features by just commenting the last line out.
  */
 
-$settings['mongodb'] = [
-  'clients' => [
-    // Client alias => constructor parameters.
-    'default' => [
-      'uri' => 'mongodb://localhost:27017',
-      'uriOptions' => [],
-      'driverOptions' => [],
+$configureMongoDb = function (array $settings): array {
+  $settings['mongodb'] = [
+    'clients' => [
+      // Client alias => connection constructor parameters.
+      'default' => [
+        'uri' => 'mongodb://localhost:27017',
+        'uriOptions' => [],
+        'driverOptions' => [],
+      ],
     ],
-  ],
-  'databases' => [
-    // Collection alias => [ client_alias, collection_name ]
-    'default' => ['default', 'drupal'],
-    'logger' => ['default', 'logger'],
-  ],
-];
+    'databases' => [
+      // Database alias => [ client_alias, database_name ]
+      'default' => ['default', 'drupal'],
+      'logger' => ['default', 'logger'],
+    ],
+  ];
+
+  return $settings;
+};
+
+$settings = $configureMongoDb($settings);
