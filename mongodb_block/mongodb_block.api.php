3b772a7e47ee0fa45e1d88464ca47d5a43c82df7
mikkeX <mikkeX@382892.no-reply.drupal.org>
2018-07-17 22:05:40 +0200

    Issue #1163584 by langworthy, MiSc: reduce mongodb_block.module conflicts with block.module
    - One pass of coding standards on the API file.

--- mongodb_block/mongodb_block.api.php
+++ mongodb_block/mongodb_block.api.php
@@ -13,37 +13,45 @@
 /**
  * Define all blocks provided by the module.
  *
- * Any module can export a block (or blocks) to be displayed by defining
- * the _block hook. This hook is called by theme.inc to display a block,
- * and also by block.module to procure the list of available blocks.
+ * This hook declares to Drupal what blocks are provided by your module and can
+ * optionally specify initial block configuration settings.
  *
  * @return array
- *   An associative array whose keys define the $delta
- *   for each block and whose values contain the block descriptions. Each
- *   block description is itself an associative array, with the following
- *   key-value pairs:
- *   - 'status': TRUE, otherwise this block is ignored.
- *   - 'region': Required name of the region this block is in.
+ *   An associative array whose keys define the delta for each block and whose
+ *   values contain the block descriptions. Each block description is itself an
+ *   associative array, with the following key-value pairs:
+ *   - cache: (optional) A bitmask describing what kind of caching is
+ *     appropriate for the block. Drupal provides the following bitmask
+ *     constants for defining cache granularity:
+ *     - DRUPAL_CACHE_PER_ROLE (default): The block can change depending on the
+ *       roles the user viewing the page belongs to.
+ *     - DRUPAL_CACHE_PER_USER: The block can change depending on the user
+ *       viewing the page. This setting can be resource-consuming for sites with
+ *       large number of users, and should only be used when
+ *       DRUPAL_CACHE_PER_ROLE is not sufficient.
+ *     - DRUPAL_CACHE_PER_PAGE: The block can change depending on the page being
+ *       viewed.
+ *     - DRUPAL_CACHE_GLOBAL: The block is the same for every user on every page
+ *       where it is visible.
+ *     - DRUPAL_CACHE_CUSTOM: The module implements its own caching system.
+ *     - DRUPAL_NO_CACHE: The block should not get cached.
+ *   - weight: (optional) Initial value for the ordering weight of this block.
+ *     Most modules do not provide an initial value, and any value provided can
+ *     be modified by a user on the block configuration screen.
+ *   - status: (optional) Initial value for block enabled status. (1 = enabled,
+ *     0 = disabled). Most modules do not provide an initial value, and any
+ *     value provided can be modified by a user on the block configuration
+ *     screen.
+ *   - region: (optional) Initial value for theme region within which this
+ *     block is set. Most modules do not provide an initial value, and any value
+ *     provided can be modified by a user on the block configuration screen.
+ *     Note: If you set a region that isn't available in the currently enabled
+ *     theme, the block will be disabled.
  *   - 'pages': an array of menu paths where the block should appear.
  *     Example: array('admin', 'node/%/edit') will display the block on admin,
  *     admin/structure etc. and the node edit pages.
  *   - 'node_type': List of node types. The block will appear only on node/%
  *     pages with the specified node types.
- *   - 'cache': A bitmask of flags describing how the block should behave with
- *     respect to block caching. The following shortcut bitmasks are provided
- *     as constants in common.inc:
- *     - DRUPAL_CACHE_PER_ROLE (default): The block can change depending on the
- *       roles the user viewing the page belongs to.
- *     - DRUPAL_CACHE_PER_USER: The block can change depending on the user
- *       viewing the page. This setting can be resource-consuming for sites
- *       with large number of users, and should only be used when
- *       DRUPAL_CACHE_PER_ROLE is not sufficient.
- *     - DRUPAL_CACHE_PER_PAGE: The block can change depending on the page
- *       being viewed.
- *     - DRUPAL_CACHE_GLOBAL: The block is the same for every user on every
- *       page where it is visible.
- *     - DRUPAL_NO_CACHE: The block should not get cached.
- *   - 'weight': explicit weight of the block.
  */
 function hook_block_info() {
   $blocks['exciting'] = array(
@@ -69,28 +77,18 @@ function hook_block_info() {
  * time.
  *
  * @param array $blocks
- *   Assocative array, key is the module name, value is the hook_block_info
+ *   Associative array, key is the module name, value is the hook_block_info
  *   returned by the module. As Drupal core and contrib rarely sets status
  *   and region this hook should be used to enable those.
  */
-function hook_mongodb_block_info_alter(&$blocks) {
+function hook_mongodb_block_info_alter(array &$blocks) {
   // Enable the management block.
   $blocks['system']['management']['status'] = 1;
   $blocks['system']['management']['region'] = 'sidebar_first';
 }
 
 /**
- * Process the block when enabled in a region in order to view its contents.
- *
- * @param array $delta
- *   Which block to return. This is a descriptive string used to identify
- *   blocks within each module and also within the theme system.
- *   The $delta for each block is defined within the array that your module
- *   returns when the hook_block_info() implementation is called.
- *
- * @return array
- *   An array which must define a 'subject' element and a 'content' element
- *   defining the block indexed by $delta.
+ * Return a rendered or renderable view of a block.
  *
  * The functions mymodule_display_block_exciting and _amazing, as used in the
  * example, should of course be defined somewhere in your module and return the
@@ -98,6 +96,23 @@ function hook_mongodb_block_info_alter(&$blocks) {
  * no block will be displayed even if "subject" is present.
  *
  * For a detailed usage example, see block_example.module.
+ *
+ * @param string $delta
+ *   Which block to render. This is a unique identifier for the block
+ *   within the module, defined in hook_block_info().
+ *
+ * @return array
+ *   Either an empty array so the block will not be shown or an array containing
+ *   the following elements:
+ *   - subject: The default localized title of the block. If the block does not
+ *     have a default title, this should be set to NULL.
+ *   - content: The content of the block's body. This may be a renderable array
+ *     (preferable) or a string containing rendered HTML content. If the content
+ *     is empty the block will not be shown.
+ *
+ * @see hook_block_info()
+ * @see hook_block_view_alter()
+ * @see hook_block_view_MODULE_DELTA_alter()
  */
 function hook_block_view($delta = '') {
   switch ($delta) {
@@ -128,22 +143,23 @@ function hook_block_view($delta = '') {
  * specific block.
  *
  * @param array $data
- *   An array of data, as returned from the hook_block_view() implementation of
- *   the module that defined the block:
- *   - subject: The localized title of the block.
+ *   The data as returned from the hook_block_view() implementation of the
+ *   module that defined the block. This could be an empty array or NULL value
+ *   (if the block is empty) or an array containing:
+ *   - subject: The default localized title of the block.
  *   - content: Either a string or a renderable array representing the content
  *     of the block. You should check that the content is an array before trying
  *     to modify parts of the renderable structure.
  * @param object $block
  *   The block object, as loaded from the database, having the main properties:
  *   - module: The name of the module that defined the block.
- *   - delta: The identifier for the block within that module, as defined within
- *     hook_block_info().
+ *   - delta: The unique identifier for the block within that module, as defined
+ *     in hook_block_info().
  *
  * @see hook_block_view_alter()
  * @see hook_block_view()
  */
-function hook_block_view_alter(&$data, $block) {
+function hook_block_view_alter(array &$data, $block) {
   // Remove the contextual links on all blocks that provide them.
   if (is_array($data['content']) && isset($data['content']['#contextual_links'])) {
     unset($data['content']['#contextual_links']);
@@ -166,8 +182,9 @@ function hook_block_view_alter(&$data, $block) {
  * implementations of hook_block_view_alter(), regardless of the module order.
  *
  * @param array $data
- *   An array of data, as returned from the hook_block_view() implementation of
- *   the module that defined the block:
+ *   The data as returned from the hook_block_view() implementation of the
+ *   module that defined the block. This could be an empty array or NULL value
+ *   (if the block is empty) or an array containing:
  *   - subject: The localized title of the block.
  *   - content: Either a string or a renderable array representing the content
  *     of the block. You should check that the content is an array before trying
@@ -175,13 +192,13 @@ function hook_block_view_alter(&$data, $block) {
  * @param object $block
  *   The block object, as loaded from the database, having the main properties:
  *   - module: The name of the module that defined the block.
- *   - delta: The identifier for the block within that module, as defined within
- *     hook_block_info().
+ *   - delta: The unique identifier for the block within that module, as defined
+ *     in hook_block_info().
  *
  * @see hook_block_view_alter()
  * @see hook_block_view()
  */
-function hook_block_view_MODULE_DELTA_alter(&$data, $block) {
+function hook_block_view_MODULE_DELTA_alter(array &$data, $block) {
   // This code will only run for a specific block. For example, if MODULE_DELTA
   // in the function definition above is set to "mymodule_somedelta", the code
   // will only run on the "somedelta" block provided by the "mymodule" module.
