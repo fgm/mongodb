a6bd5dd24ce952876afe70b40c9d234e3e846558
Kalaiselvan Swamy <KalaiselvanSwamy@2575370.no-reply.drupal.org>
2018-09-15 15:47:45 +0530

    Issue #2644482 - review comments #51 addressed

--- src/Install/Requirements.php
+++ src/Install/Requirements.php
@@ -9,29 +9,31 @@
 
 namespace Drupal\mongodb\Install;
 
+use \Alcaeus\MongoDbAdapter\TypeInterface::class as AlcaeusAdapter;
+
 /**
  * Class Requirements implements hook_requirements().
  */
 class Requirements {
   public function __construct() {
-    $this->extension_name = '';
-    $this->requirements = array();
+    protected $extension_name = '';
+    protected $requirements = array();
 
     // Ensure translations don't break at install time.
-    $this->t = get_t();
+    protected $t = get_t();
   }
 
 /**
  * Common entry point for all requirement checks.
  */
-  public static function check($phase) {
+  public function check($phase) {
     // Check extension version
     $this->checkExtension();
 
     // Check server version
     $this->checkServer($phase);
 
-    return $this->requirements;
+    return $requirements;
   }
 
 /**
@@ -39,17 +41,17 @@ class Requirements {
  */
   public function checkExtension() {
     if (extension_loaded('mongodb')) {
-      $this->extension_name = 'mongodb';
+      $extension_name = 'mongodb';
       if (class_exists(MongoClient::class)) {
-        $this->requirements['mongodb_extension'] += array(
-          'value' => t('Properly replaced'),
+        $requirements['mongodb_extension'] += array(
+          'value' => $t('Properly replaced'),
           'description' => $t('The mongo-php-adapter library and the ext-mongodb extension are present, replacing the ext-mongo extension.'),
           'severity' => REQUIREMENT_OK,
         );
       }
       else {
-        $this->requirements['mongodb_extension'] += array(
-          'value' => t('Insufficient'),
+        $requirements['mongodb_extension'] += array(
+          'value' => $t('Insufficient'),
           'description' => $t('The ext-mongodb extension is present, but the mongo-php-adapter is missing, so it cannot replace the old ext-mongo extension.'),
           'severity' => REQUIREMENT_ERROR,
         );
@@ -57,27 +59,27 @@ class Requirements {
     }
     elseif (!extension_loaded('mongo')) {
       if (class_exists(MongoClient::class)) {
-        $this->requirements['mongodb_extension'] += array(
-          'value' => t('Insufficient'),
+        $requirements['mongodb_extension'] += array(
+          'value' => $t('Insufficient'),
           'description' => $t('The mongo-php-adapter library is present, but the ext-mongodb is missing, so it cannot replace the old ext-mongo extension.'),
           'severity' => REQUIREMENT_ERROR,
         );
       }
       else {
-        $this->requirements['mongodb_extension'] += array(
-          'value' => t('Insufficient'),
+        $requirements['mongodb_extension'] += array(
+          'value' => $t('Insufficient'),
           'description' => $t('Neither the old ext-mongo nor the new ext-mongodb extension nor the mongo-php-adapter library are present.'),
           'severity' => REQUIREMENT_ERROR,
         );
       }
     }
     else {
-      $this->extension_name = 'mongo';
-      if (class_exists(\Alcaeus\MongoDbAdapter\TypeInterface::class)) {
-        $this->requirements['mongodb_library_redundant'] = array(
+      $extension_name = 'mongo';
+      if (class_exists(AlcaeusAdapter)) {
+        $requirements['mongodb_library_redundant'] = array(
           'title' => 'mongo-php-adapter',
-          'value' => t('Redundant'),
-          'description' => t('The mongo-php-adapter library is installed while the old ext-mongo extension is present. This does not hurt, but is somewhat redundant.'),
+          'value' => $t('Redundant'),
+          'description' => $t('The mongo-php-adapter library is installed while the old ext-mongo extension is present. This does not hurt, but is somewhat redundant.'),
           'severity' => REQUIREMENT_WARNING,
         );
       }
@@ -88,40 +90,46 @@ class Requirements {
  * Checks mongodb server version.
  */
   public function checkServer($phase) {
-    $this->extension = new ReflectionExtension($this->extension_name);
-    $this->version = $this->extension->getVersion();
-    $this->requirements['mongodb_extension'] += array(
-      'value' => $this->version,
+    try {
+      $extension = new ReflectionExtension($extension_name);
+      $version = $extension->getVersion();
+    } catch (ReflectionException $exception) {
+      watchdog('mongodb',$t('Reflection error : %reflection_exception'),
+        array('%reflection_exception' => $exception), WATCHDOG_ERROR);
+      );
+    }
+    $requirements['mongodb_extension'] += array(
+      'value' => $version,
       'severity' => REQUIREMENT_OK,
     );
 
     // Server versions prior to 1.3 do not support findAndModify command
     // needed by mongodb_next_id() function.
-    $this->min_version = '1.3';
+    $min_version = '1.3';
 
     // During install, the module is not yet loaded by core.
     if ($phase == 'install') {
       require_once __DIR__ . '/../../../mongodb.module';
     }
-    $this->db = mongodb();
+    $db = mongodb();
 
-    $this->requirements['mongodb_server'] = array(
-      'title' => $this->t('MongoDB server'),
+    $requirements['mongodb_server'] = array(
+      'title' => $t('MongoDB server'),
     );
-    $this->return = $this->db->execute('db.version();');
-    $this->version = $this->return['retval'];
-    if (version_compare($this->version, $this->min_version) == -1) {
-      $this->requirements['mongodb_server'] += array(
-        'value' => $this->version,
-        'description' => $this->t('MongoDB module requires MongoDB server version @min_version or higher.', array(
-          '@min_version' => $this->min_version,
+    $return = $db->execute('db.version();');
+    $version = $return['retval'];
+    if (version_compare($version, $min_version) == -1) {
+      $requirements['mongodb_server'] += array(
+        'value' => $version,
+        'description' => $t('MongoDB module requires MongoDB server version @min_version or higher.', array(
+          '@min_version' => $min_version,
         )),
         'severity' => REQUIREMENT_ERROR,
       );
     }
     else {
-      $this->requirements['mongodb_server'] += array(
-        'value' => $this->version,
+      $requirements['mongodb_server'] += array(
+        'value' => $version,
         'severity' => REQUIREMENT_OK,
       );
     }
